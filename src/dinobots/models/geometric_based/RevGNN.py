7e70dc48eff5ab7683636eee2a83a21ae981bc3e
Keshav Dial <y.inuyasha@gmail.com>
2022-09-25 18:20:15 -0400

    refactor

---
+++ src/dinobots/models/geometric_based/RevGNN.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn.functional as F
+from torch.nn import LayerNorm, Linear
+from torch_geometric.nn import GroupAddRev, SAGEConv
+
+
+class RevGNN(torch.nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        hidden_channels,
+        out_channels,
+        num_layers,
+        dropout,
+        num_groups=2,
+    ):
+        super().__init__()
+
+        self.dropout = dropout
+
+        self.lin1 = Linear(in_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, out_channels)
+        self.norm = LayerNorm(hidden_channels, elementwise_affine=True)
+
+        assert hidden_channels % num_groups == 0
+        self.convs = torch.nn.ModuleList()
+        for _ in range(num_layers):
+            conv = GNNBlock(
+                hidden_channels // num_groups,
+                hidden_channels // num_groups,
+            )
+            self.convs.append(GroupAddRev(conv, num_groups=num_groups))
+
+    def reset_parameters(self):
+        self.lin1.reset_parameters()
+        self.lin2.reset_parameters()
+        self.norm.reset_parameters()
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, edge_index):
+        x = self.lin1(x)
+
+        # Generate a dropout mask which will be shared across GNN blocks:
+        mask = None
+        if self.training and self.dropout > 0:
+            mask = torch.zeros_like(x).bernoulli_(1 - self.dropout)
+            mask = mask.requires_grad_(False)
+            mask = mask / (1 - self.dropout)
+
+        for conv in self.convs:
+            x = conv(x, edge_index, mask)
+        x = self.norm(x).relu()
+        x = F.dropout(x, p=self.dropout, training=self.training)
+        return self.lin2(x)
+
+
+class GNNBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.norm = LayerNorm(in_channels, elementwise_affine=True)
+        self.conv = SAGEConv(in_channels, out_channels)
+
+    def reset_parameters(self):
+        self.norm.reset_parameters()
+        self.conv.reset_parameters()
+
+    def forward(self, x, edge_index, dropout_mask=None):
+        x = self.norm(x).relu()
+        if self.training and dropout_mask is not None:
+            x = x * dropout_mask
+        return self.conv(x, edge_index)
